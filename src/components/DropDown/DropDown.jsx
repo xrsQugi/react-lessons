cefe96811960ac4b1e93f4a5eebe2f05295b9ab1
xrsQugi <02.04.5.art@gmail.com>
2023-07-18 19:24:38 +0300

    fix

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -14,32 +14,16 @@ export default class DropDown extends Component {
     }
 
     Toggle = () => {
-        this.setState((prewState) => ({
-            visible: !prewState.visible,
+        this.setState((prevState) => ({
+            visible: !prevState.visible,
         }))
     }
-
-    // ! замінили цю частину на функцію toggle 
-    // Show = () => {
-    //     this.setState({
-    //         visible: true,
-    //     })
-    // }
-
-    // Hide = () => {
-    //     this.setState({
-    //         visible: false,
-    //     })
-    // }
-    //!
 
     render() {
         const {visible} = this.state;
         return (
         <div className={css.drop_down}>
             <button type="button" className={css.drop_down_button} onClick={this.Toggle}>{visible ? "Hide" : "Show"}</button>
-            {/* <button type="button" className={css.drop_down_button} onClick={this.Toggle}>Show</button>
-            <button type="button" className={css.drop_down_button} onClick={this.Toggle}>Hide</button> */}
             {visible && 
                 <div className={css.drop_down_menu}>
                     <Clock></Clock>

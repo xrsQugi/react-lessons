e160bbda6f7852eb6e853c7ff58cd17e6711bf9b
xrsQugi <02.04.5.art@gmail.com>
2023-07-31 11:50:11 +0300

    dropdown and live cycle

--- src/pages/DropDownPage/DropDownPage.jsx
+++ src/pages/DropDownPage/DropDownPage.jsx
@@ -1,10 +1,11 @@
 import DropDown from '../../components/DropDown/DropDown';
 import LifeCycle from '../../components/LifeCycle/LifeCycle';
 import Section from '../../components/Section/Section';
+import css from './DropDownPage.module.css';
 
 export default function DropDownPage(params) {
   return (
-    <div>
+    <div className={css.container}>
       <Section title="Dropdown">
         <DropDown />
       </Section>
